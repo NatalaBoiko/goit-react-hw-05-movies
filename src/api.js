7e4c87d6e195ddd2ae2ca05ee8d71ab4b138ce0e
NatalaBoiko <natalabojko@gmail.com>
2022-08-07 01:20:20 +0300

    Movies

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 export const fetchTrending = async () => {
   return axios
-    .get(`trending/all/day?`)
+    .get(`/trending/all/day?`)
     .then(({ data: { results } }) =>
       results.map(({ id, title, poster_path: poster }) => ({
         id,
@@ -22,6 +22,20 @@ export const fetchTrending = async () => {
     });
 };
 
+export const fetchByQuery = async query => {
+  return axios
+    .get(`/search/movie?query=${query}`)
+    .then(({ data: { results } }) =>
+      results.map(({ id, title, poster_path: poster }) => ({
+        id,
+        title,
+        poster: IMG_URL + poster,
+      }))
+    )
+    .catch(error => {
+      console.log(error.message);
+    });
+};
 // https://api.themoviedb.org/3/trending/all/day?api_key=2123fea73978ac77fe6a3f347cf7ced6
 
 // https://api.themoviedb.org/3/search/movie?api_key=2123fea73978ac77fe6a3f347cf7ced6&language=en-US&page=1&include_adult=false
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,13 +1,99 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams, Link, useLocation } from 'react-router-dom';
+import { fetchByQuery } from '../../api';
+
 const Movies = () => {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams('');
+  const location = useLocation();
+
+  const searchQuery = searchParams.get('query');
+
+  useEffect(() => {
+    searchQuery && fetchByQuery(searchQuery).then(setMovies);
+  }, [searchQuery]);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetchByQuery(query);
+    setMovies(response);
+    setSearchParams({ query });
+    setQuery('');
+  };
+  const onChange = e => {
+    setQuery(e.target.value);
+  };
+
   return (
     <>
-      <h2>Movies</h2>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={query} onChange={onChange} />
         <button type="submit">search</button>
       </form>
+      {movies && (
+        <ul>
+          {movies.map(({ id, title, poster }) => (
+            <li key={id}>
+              <Link to={`/movies/${id}`} state={{ from: location }}>
+                <img src={poster} alt={title} />
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
 
 export default Movies;
+
+// import { useState, useEffect } from 'react';
+// import { useSearchParams, Link, useLocation } from 'react-router-dom';
+// import { fetchByQuery } from '../../api';
+
+// const Movies = () => {
+//   const [movies, setMovies] = useState([]);
+//   const [searchParams, setSearchParams] = useSearchParams('');
+//   const location = useLocation();
+//   const query = searchParams.get('query');
+
+//   useEffect(() => {
+//     query && fetchByQuery(query).then(setMovies);
+//   }, [query]);
+
+//   const handleSubmit = async e => {
+//     e.preventDefault();
+//     const response = await fetchByQuery(query);
+//     setMovies([...response]);
+//     setSearchParams('');
+//   };
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={query}
+//           onChange={e => setSearchParams({ query: e.target.value })}
+//         />
+//         <button type="submit">search</button>
+//       </form>
+//       {movies && (
+//         <ul>
+//           {movies.map(({ id, title, poster }) => (
+//             <li key={id}>
+//               <Link to={`/movies/${id}`} state={{ from: location }}>
+//                 <img src={poster} alt={title} />
+//                 {title}
+//               </Link>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </>
+//   );
+// };
+
+// export default Movies;
