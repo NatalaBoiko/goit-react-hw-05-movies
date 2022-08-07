c8794aa799deba1ea7dc9034741d6bb726f441a6
NatalaBoiko <92225403+NatalaBoiko@users.noreply.github.com>
2022-08-07 21:05:20 +0000

    Deploying to gh-pages from @ NatalaBoiko/goit-react-hw-05-movies@1e3bae3d99da1a64d3eb56536e86c08d41e7b317 🚀

---
+++ static/js/805.ea70c957.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{9805:function(e,n,t){t.r(n);var o=t(501),r=t(184);n.default=function(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)("h2",{children:"Oops... nothing found..."}),(0,r.jsx)(o.rU,{to:"/",children:"Back to home"})]})}}}]);
+//# sourceMappingURL=805.ea70c957.chunk.js.map
